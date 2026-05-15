d6a75be9281618b642a1b3cbec365d29eccd3d09
Lando <lando_vargas@outlook.com>
2026-05-15 02:00:50 -0400

    CH6 L6

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -3,6 +3,9 @@ package auth
 import (
 	"fmt"
 	"testing"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 func TestAuthHashCompare(t *testing.T) {
@@ -85,6 +88,81 @@ func TestCheckPasswordHash(t *testing.T) {
 			}
 			if !tt.wantErr && match != tt.matchPassword {
 				t.Errorf("CheckPasswordHash() expects %v, got %v", tt.matchPassword, match)
+			}
+		})
+	}
+}
+
+func TestJWT(t *testing.T) {
+	// First, we need to create some hashed passwords for testing
+	userId1, err := uuid.NewUUID()
+
+	if err != nil {
+		t.Error(err)
+	}
+	token1, err := MakeJWT(userId1, "first secret", time.Duration(10*time.Minute))
+	if err != nil {
+		t.Error(err)
+	}
+
+	userId2, err := uuid.NewUUID()
+	if err != nil {
+		t.Error(err)
+	}
+
+	token2, err := MakeJWT(userId2, "secondo secret", time.Duration(-10*time.Minute))
+	if err != nil {
+		t.Error(err)
+	}
+
+	userId3, err := uuid.NewUUID()
+	if err != nil {
+		t.Error(err)
+	}
+
+	token3, err := MakeJWT(userId3, "right password", time.Duration(10*time.Minute))
+	if err != nil {
+		t.Error(err)
+	}
+
+	tests := []struct {
+		name        string
+		userId      uuid.UUID
+		tokenSecret string
+		token       string
+		wantErr     bool
+	}{
+		{
+			name:        "Valid Token",
+			userId:      userId1,
+			tokenSecret: "first secret",
+			token:       token1,
+			wantErr:     false,
+		},
+		{
+			name:        "Stale Token",
+			userId:      userId2,
+			tokenSecret: "secondo secret",
+			token:       token2,
+			wantErr:     true,
+		},
+		{
+			name:        "Wrong Secret",
+			userId:      userId3,
+			tokenSecret: "wrong password",
+			token:       token3,
+			wantErr:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			match, err := ValidateJWT(tt.token, tt.tokenSecret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && match != tt.userId {
+				t.Errorf("ValidateJWT() expects %v, got %v", tt.userId, match)
 			}
 		})
 	}
